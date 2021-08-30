9d0074d16513b13dd312401f999c66d47b5c4112
christine318 <89437543+christinemanoukian@users.noreply.github.com>
2021-08-30 06:58:06 +0000

    Finished Assigment.

--- 001/code.py
+++ 001/code.py
@@ -38,3 +38,75 @@ def is_prime(n):
       if n % num == 0:
         return "Not Prime"
     return "Prime"
+
+
+def get_intersection(list1, list2):
+  result = []
+  for element in list1:
+    if element in list2 and element not in result:
+      result.append(element)
+  return result
+
+
+def get_union(list1, list2):
+  result = []
+  for element in list1:
+    if element not in result:
+      result.append(element)
+  for elements in list2:
+    if elements not in result:
+      result.append(elements)
+  return result
+
+
+def count_characters(input_string):
+  new = input_string.lower()
+  result = {
+
+  }
+  for char in new:
+      result[char] = result.get(char, 0) + 1
+  return result
+
+
+def get_first_nth_terms_nonrecursive(n):
+  result = []
+  for num in range(1, n):
+    if num == 1:
+      first_term = 5
+      result.append(5)
+    result.append(3 * first_term - 4)
+    first_term = 3 * first_term - 4
+  return result
+
+
+def get_nth_term_recursive(n):
+  if n == 1:
+    return 5
+  answer = 3 * get_nth_term_recursive(n-1) - 4
+  return answer
+
+
+def convert_to_base_10(inputt):
+  total = 0
+  count = 0
+  lst = [int(num) for num in str(inputt)]
+  lst = lst[::-1]
+  for number in lst:
+    if number == 1:
+      total += 2 ** count
+    count += 1
+  return total
+
+
+import math
+def convert_to_base_2(inputt):
+  var = math.floor(math.log(inputt, 2)) + 1
+  result = ""
+  for char in range(var, 0, -1):
+    if inputt - 2 ** (char - 1) >= 0:
+      result += "1"
+      inputt -= 2 ** (char - 1)
+    else:
+      result += "0"
+  return int(result)
--- 001/tests.py
+++ 001/tests.py
@@ -29,3 +29,35 @@ if is_prime(15) != "Not Prime":
   print("is_prime failed on input 15")
 if is_prime(4) != "Not Prime":
   print("is_prime failed on input 4")
+
+from code import get_intersection
+if get_intersection([1,2,3,4], [1,5,6,2,7]) != [1,2]:
+  print("get_intersection failed on input [1,2,3,4], [1,5,6,2,7]")
+if get_intersection([1,1,1,2,3,4], [1,1,5,6]) != [1]:
+  print("get_intersection failed on input [1,1,1,2,3,4], [1,1,5,6]")
+if get_intersection([1,2,3], [4,3,3,7]) != [3]:
+  print("get_intersection failed on input [1,2,3], [4,3,3,7]")
+
+from code import get_union
+if get_union([1,2,3,4], [1,5,6,2,7]) != [1,2,3,4,5,6,7]:
+  print("get_union failed on input [1,2,3,4], [1,5,6,2,7]")
+if get_union([1,1,1,2,3,4], [1,1,5,6]) != [1,2,3,4,5,6]:
+  print("get_union failed on input [1,1,1,2,3,4], [1,1,5,6]")
+if get_union([1,2,3], [4,3,3,7]) != [1,2,3,4,7]:
+  print("get_union failed on input [1,2,3], [4,3,3,7]")
+
+from code import get_first_nth_terms_nonrecursive
+if get_first_nth_terms_nonrecursive(3) != [5,11,29]:
+  print("get_first_nth_terms_nonrecursive failed on input 3")
+
+from code import get_nth_term_recursive
+if get_nth_term_recursive(3) != 29:
+  print("get_nth_term_recursive failed on input 3")
+
+from code import convert_to_base_10
+if convert_to_base_10(10011) != 19:
+  print("convert_to_base_10 failed on input 10011")
+
+from code import convert_to_base_2
+if convert_to_base_2(19) != 10011:
+  print("convert_to_base_2 failed on input 19")
