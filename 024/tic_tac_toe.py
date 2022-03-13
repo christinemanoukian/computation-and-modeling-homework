bfb31b0f8e23ad4e33add5e2cf44430397a9c262
christinemanoukian <christinemanoukian18@gmail.com>
2022-03-13 02:13:33 +0000

    need help with setting winner

--- 024/tic_tac_toe.py
+++ 024/tic_tac_toe.py
@@ -11,8 +11,6 @@ class Player:
             for j in range(len(board[i])):
                 if board[i][j] == 0:
                     open_spaces.append((i,j))
-        if open_spaces == []:
-            return 'tie'
         else:
             return random.choices(open_spaces)[0]
 
@@ -31,33 +29,32 @@ class Game:
         i,j = player.choose_move(self.board)
         self.board[i][j] = player.player_number
 
-
     def check_winner(self):
-        players = [self.p1, self.p2]
-        # go thru instances of a win, if it's a win, do one of the options in test file
+        open_spaces = []
+        for i in range(len(board)):
+            for j in range(len(board[i])):
+                if board[i][j] == 0:
+                    open_spaces.append((i,j))
+        if open_spaces == []:
+            self.winner = 'tie'
+        else:
+        # go thru instances of a win, if it's a win, do one of the options in test file (not sure which is better or if 
+        # there's an option better that I don't know of
 
-
-    
+ 
     def run(self):
         while self.winner == None:
-            if player.choose_move(self.board) == 'tie':
-                return 'tie'
-                break
             self.move(self.p1)
             self.check_winner()
             if self.winner != None:
                 return self.winner
-                break
-            if player.choose_move(self.board) == 'tie':
-                return 'tie'
-                break
-            self.move(self.p2)
-            self.check_winner()
-            if self.winner != None:
-                return self.winner
-                break
+            else:
+                self.move(self.p2)
+                self.check_winner()
+                if self.winner != None:
+                    return self.winner
 
-# ask about log= true
+# ask about log=true
 # code tie instance
 
 # turn board into matrix just for fun
@@ -68,6 +65,4 @@ class Game:
 player1 = Player()
 player2 = Player()
 game = Game(player1, player2)
-game.test()
-print(game.winner)
-#game.run()
+print(game.run())
