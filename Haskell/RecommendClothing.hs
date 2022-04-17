e9cca1691ab99c3e55acd0a7b223eec34d634ebd
christine318 <89437543+christinemanoukian@users.noreply.github.com>
2022-04-17 04:20:20 +0000

    check

--- Haskell/RecommendClothing.hs
+++
@@ -1,9 +0,0 @@
-recommendClothing :: (RealFloat a) => a -> String  
-recommendClothing degreesCelsius  
-    | degreesFahrenheit >= 80 = "I recommend wearing a shortsleeve shirt"
-    | degreesFahrenheit >= 65 = "I recommend wearing a longsleeve shirt"
-    | degreesFahrenheit >= 50 = "I recommend wearing a sweater"
-    | otherwise = "I recommend wearing a jacket"
-    where degreesFahrenheit = (degreesCelsius*(9/5))+32
-
-main = print(recommendClothing 1)
